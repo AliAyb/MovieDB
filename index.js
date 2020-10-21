e0d21f0d56655bdb95a4712f06028bea36dbaaf8
Ali <alii.aybb@gmail.com>
2020-10-21 16:25:53 +0300

    Step 6

--- index.js
+++ index.js
@@ -60,5 +60,18 @@ const movies = [
 app.get('/movies/read', (req, res) => {
     res.status(200).send(movies);
 });
+app.get('/movies/read/by-date', (req, res) => {
+    movies.sort(function(a, b){return a.year - b.year});
+    res.status(200).send(movies);
+})
+app.get('/movies/read/by-rating', (req, res) => {
+    movies.sort(function(a, b){return b.rating - a.rating});
+    res.status(200).send(movies);
+})
+app.get('/movies/read/by-title', (req, res) => {
+    movies.sort(function(a, b){return a.title.localeCompare(b.title)});
+    res.status(200).send(movies);
+})
+
 
 
