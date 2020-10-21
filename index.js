7c74fbc0600a49ddb2ea90fb2fb9329c7026bdbf
Ali <alii.aybb@gmail.com>
2020-10-21 14:44:17 +0300

    Step 4

--- index.js
+++ index.js
@@ -27,7 +27,20 @@ app.get('/time', (req, res) => {
             hours = "" + today.getHours();
         }
     var time = today.getHours() + ":" + today.getMinutes(); 
-    res.send('status:200, message:'+time); 
+    res.send('status:200, message: '+time); 
+})
+
+app.get('/hello/:id',(req, res) => {
+    res.send('status:200, message: Hello, ' + req.params.id);
+})
+
+app.get('/search', (req, res) => {
+if (req.query.s == ''){
+    res.status(500).send('error:true, you have to provide a search')
+}
+else {
+    res.status(200).send('ok, data: '+req.query.s)
+}
 })
 
 
