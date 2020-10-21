78b77493dd9db9b0fa750219e3c5aa9c46ba8aa7
Ali <alii.aybb@gmail.com>
2020-10-21 13:28:35 +0300

    Step 3

--- index.js
+++ index.js
@@ -9,3 +9,25 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+app.get('/test', (req, res) => {
+    res.send('status:200, message:ok')
+})
+
+app.get('/time', (req, res) => {
+    var today = new Date(); 
+    if(today.getMinutes() < 10){
+        minutes = "0" + today.getMinutes();
+        }else{
+            minutes = "" + today.getMinutes();
+        }
+        if(today.getHours() < 10){
+            hours = "0" + today.getHours();
+        }else{
+            hours = "" + today.getHours();
+        }
+    var time = today.getHours() + ":" + today.getMinutes(); 
+    res.send('status:200, message:'+time); 
+})
+
+
